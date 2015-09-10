fe7f0a28cc43db545e171bd939bc3e99c82360ab
LisaLoop <lisa.lehrman@gmail.com>
2015-09-10 14:12:03 -0700

    comparing cards

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
-var $last_clicked = null;
-var clicks = 0;
+// var $last_clicked = null;
+// var clicks = 0;
 // var counter = 
 
 $(document).ready(function(){
+  var turnCount = 0;
+  var lastClicked;
   
   $("#reset").on("click", function shuffle(event){
       var parent = $("#shuffle");
@@ -13,15 +15,60 @@ $(document).ready(function(){
   });
 
   $(".piece").on("click", function(){
-    window.$last_clicked = $(this);
+    // increment turn count
+    turnCount++;
+    var justClicked = this;
     $(this).find(".githubImg").toggleClass("hideImg");
-    //checks that there've only been 2 clicks and behaves accordingly
-    // if (++clicks < 2){
-      
-    // }
+    // compare previous and current click items
+    console.log(lastClicked, justClicked);
+    lastClicked = justClicked;
   });
+// ONE POSSIBLE WAY TO DO THINGS
+  // var isFirstPick = true;
+  // var currentCard;
+
+
+  // $('img').click(function(e) {
+  //   var classList = e.target.classList
+
+  //   if (isFirstPick) {
+  //     console.log('this is the first pick')
+  //     currentCard = classList[classList.length - 1]
+  //     console.log(currentCard)
+  //     isFirstPick = false;
+  //     console.log('this is the second pick')
+  //   } else if (currentCard === classList[classList.length - 1]) {
+  //     isFirstPick = true;
+  //     console.log('MATCH');
+  //   } else {
+  //     isFirstPick = true;
+  //     console.log('NOT A MATCH');
+  //   }
+  // })
+  // END OF FIRST POSSIBLE WAY
+
+  // ANOTHER THING THAT MIGHT WORK
+
+//   $(function() {
+//   var cards = document.querySelectorAll(".piece");
+//   for ( var i  = 0, len = cards.length; i < len; i++ ) {
+//     var card = cards[i];
+//     clickListener( card );
+//   }
+
+//   $('A').value
+
+
+//   function clickListener(card) {
+//     card.addEventListener( "click", function() {
+//       var c = this.classList;
+//       c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+//       console.log(c);
+//     });
+//   }
+// })
+  // END OTHER THING
+
+  // })
 
 })
-
-
-
