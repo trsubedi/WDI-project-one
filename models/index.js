e1281eb56d040aab8ec0ae15c7547919d536f9fc
LisaLoop <lisa.lehrman@gmail.com>
2015-09-11 09:51:14 -0700

    added background image to tile back, border radius and new border color, adjusted button position

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 var mongoose = require("mongoose"); //connects to mongoose
 mongoose.connect("mongodb://localhost/project-one"); //to database
 
-mongoose.connect( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "YOUR OWN LOCAL URL HERE" )
+// mongoose.connect( process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "YOUR OWN LOCAL URL HERE" )
 module.exports.User = require("./user"); //export to book
 
 
